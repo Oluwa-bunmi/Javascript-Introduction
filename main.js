9e4d48cd89946fb0df665eb6282af50d614ee335
Jummy007 <gbemisoyeadejumoke@gmail.com>
2022-03-15 10:38:16 +0100

    done with the calculation

---
+++ main.js
@@ -0,0 +1,23 @@
+// let question = prompt("Enter your name: ");
+// let age = prompt("Enter your age: ");
+// let address = prompt("Enter your address: ");
+// let phone = prompt("Enter your phone number: ");
+
+// document.getElementById("name").innerHTML = question;
+// document.getElementById("age").innerHTML = age;
+// document.getElementById("address").innerHTML = address;
+// document.getElementById("phone").innerHTML = phone;
+
+
+
+// alert("Welcome to ATC" + question)
+// alert("age" + "looks good")
+
+// Calculator section
+var x = 60 
+var y = x * x
+document.getElementById("first").innerHTML = y;
+var z = 24 * y
+document.getElementById("second").innerHTML = z;
+document.getElementById("third").innerHTML = 7 * z;
+
